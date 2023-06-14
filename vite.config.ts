79eed799bd2fae3a9b09b17f8e785bd7177b1d14
Joshua <joshua+github@frontlawn.net>
2023-06-14 00:15:31 -0700

    chore(deps): Update dependencies.

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ export default vite.library({
   test: {
     deps: {
       // Interestingly, Vitest has an option that will fix the very problem we
-      // are trying to solve. In order to test this package, we need to disable
-      // it.
+      // are trying to solve. But in order to test this package, we need to
+      // disable it.
       interopDefault: false
     }
   }
